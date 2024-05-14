382b761efd9a48304d5e4bf5c667c2d866bcecf1
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-05-14 11:02:53 +0200

    removed excess files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css'; 
 import RegisterForm from './RegisterForm'; 
-
+import '../src/images/Image.png';
+import '../src/images/Logo.png';
 function App() {
     return (
         <div className="App">
+            <img src="Logo.png alt=logo"/>
             <RegisterForm />
+            <img src="Image.png alt=image"/>
         </div>
+    
     );
 }
 
